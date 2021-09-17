36ebd7b18b52ed7647faa137f154429e2fedba5a
eryan411 <eryan411@gmail.com>
2021-09-16 23:31:21 -0400

    reupload due to corrupt git

---
+++ 04_removeFromArray/removeFromArray.js
@@ -0,0 +1,14 @@
+const removeFromArray = function(firstArray, ...toRemove) {
+
+  let modifiedArray = [...firstArray];  
+
+  for(i=0; i < firstArray.length; i++){
+    if(modifiedArray.includes(toRemove[i])){
+      modifiedArray.splice(modifiedArray.indexOf(toRemove[i]), 1);
+    }
+  }
+  
+  return modifiedArray;
+};
+
+module.exports = removeFromArray;
